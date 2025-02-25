2c8c87a968c22b42ecc0a06c501bd81224ebd479
Eduardo Gonzalez Melgoza <edu.glz.mlz@gmail.com>
2025-02-25 11:48:48 +0100

    Initial commit

---
+++ AnAppleADay/AnAppleADayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnAppleADayApp.swift
+//  AnAppleADay
+//
+//  Created by Eduardo Gonzalez Melgoza on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AnAppleADayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

910997fdf2b2742a25c6496dcb2951ae3a9aef16
Davide Castaldi <148765897+Dave-Ed-Cast@users.noreply.github.com>
2025-03-31 15:19:35 +0200

    Deleted useless state variables

--- AnAppleADay/Views/ModelView.swift
+++ AnAppleADay/Views/ModelView.swift
@@ -13,18 +13,6 @@ struct ModelView: View {
     
     @Environment(AppModel.self) private var appModel
     
-//    let dataSet: DicomDataSet
-    
-    @Environment(\.setMode) private var setMode
-    
-    @State private var error: Error? = nil
-    @State private var bonesEntity: Entity? = nil
-    @State private var arteriesEntity: Entity? = nil
-    @State private var scale: Bool = false
-    @State private var bonesCenter: SIMD3<Float> = .zero
-    @State private var arteriesCenter: SIMD3<Float> = .zero
-    @State private var isLoading: Bool = true
-    
     var body: some View {
         RealityView { content in
             content.add(appModel.bonesEntityHolder!)

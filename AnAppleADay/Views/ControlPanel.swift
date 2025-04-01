d0fc07a78839e8a6ef9faaa12168bd0c8f091faa
Davide Castaldi <148765897+Dave-Ed-Cast@users.noreply.github.com>
2025-04-01 11:02:41 +0200

    Reworked and tested the other functions
    Now everything is embedded into the App Model, and also properly documented

--- AnAppleADay/Views/ControlPanel.swift
+++ AnAppleADay/Views/ControlPanel.swift
@@ -15,60 +15,33 @@ struct ControlPanel: View {
     @Environment(AppModelServer.self) private var appModelServer
     @Environment(AppModel.self) private var appModel
     @Environment(\.setMode) private var setMode
-    
-    @State private var gestures: Bool = true
-    
+        
     var body: some View {
         
             HStack{
                 //toggle bones arteries
-                Button {
-                    appModel.bonesArteriesToggle()
-                } label: {
-                    Image("hideBones")
-                }
+                Button { appModel.bonesArteriesToggle() }
+                label: { Image("hideBones") }
                 
                 //scale
-                Button {
-                    appModel.scale.toggle()
-                    if(appModel.scale){
-                        appModel.bonesEntityHolder!.scale *= 2.0
-                        appModel.arteriesEntityHolder!.scale *= 2.0
-                    }else{
-                        appModel.bonesEntityHolder!.scale /= 2.0
-                        appModel.arteriesEntityHolder!.scale /= 2.0
-                    }
-                } label: {
-                    Image("RestoreSize")
-                }
+                Button { appModel.scaleEntities() }
+                label: { Image("RestoreSize") }
                 
                 
                 //lock 3D
-                Button {
-                    gestures.toggle()
-                    toggleGestures(component: appModel.bonesEntityHolder!, isEnabled: gestures)
-                    toggleGestures(component: appModel.arteriesEntityHolder!, isEnabled: gestures)
-                } label: {
-                    Image("lockInPosition")
-                }
+                Button { appModel.toggleGestures() }
+                label: { Image("lockInPosition") }
                 
                 //Divider
                 Divider().frame(width: 5, height: 40)
                 
                 //Show 2D image
-                Button {
-                    //potentially show and hide the model so they don't overlap
-                    Task { await setMode(.inputAddress, nil) }
-                } label: {
-                    Image("connect2D")
-                }
+                Button { Task { await setMode(.inputAddress, nil) } }
+                label: { Image("connect2D") }
                 
-                //lock 2D image
-                Button {
-                    appModelServer.hideBar.toggle()
-                } label: {
-                    Image("lock2d")
-                }
+                
+                Button { appModel.hideBar.toggle() }
+                label: { Image("lock2d") }
                 
                 //LOCKINTO3D2
                 Button {
@@ -85,13 +58,5 @@ struct ControlPanel: View {
             .persistentSystemOverlays(.visible)
             
         
-    }
-    
-    func toggleGestures(component: Entity, isEnabled: Bool) {
-        component.gestureComponent?.canDrag = isEnabled
-        component.gestureComponent?.canRotate = isEnabled
-        component.gestureComponent?.canScale = isEnabled
-        component.gestureComponent?.pivotOnDrag = isEnabled
-        component.gestureComponent?.preserveOrientationOnPivotDrag = isEnabled
     }
 }
